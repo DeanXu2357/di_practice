553e132a485371a268927c13b45644ab6ce9da59
Dean Xu <dean.xu.2357@gmail.com>
2022-05-23 18:29:35 +0800

    v2

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module di_practice
 go 1.18
 
 require (
+	github.com/slack-go/slack v0.10.3
 	gorm.io/driver/sqlite v1.3.2
 	gorm.io/gorm v1.23.5
 )
 
 require (
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.12 // indirect
--- service/authentication/authentication.go
+++ service/authentication/authentication.go
@@ -3,10 +3,12 @@ package authentication
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -28,6 +30,20 @@ type AccountModel struct {
 type authentication struct{}
 
 func (a *authentication) Verify(accountID, pwd, otp string) (bool, error) {
+	// check if locked
+	res, err := http.Get(fmt.Sprintf("https://is_locked/%s", accountID))
+	if err != nil {
+		return false, fmt.Errorf("http error: %w", err)
+	}
+	rtnBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return false, fmt.Errorf("parse response error: %w", err)
+	}
+	isLocked := string(rtnBytes)
+	if isLocked == "true" {
+		return false, errors.New("account locked")
+	}
+
 	// get pwd from db
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
@@ -43,27 +59,72 @@ func (a *authentication) Verify(accountID, pwd, otp string) (bool, error) {
 	hash := sha256.Sum256([]byte(pwd))
 	hashedPwd := hex.EncodeToString(hash[:])
 
-	if hashedPwd != pwdFromDB {
-		return false, nil
-	}
-
 	// get opt by http request
-	res, err := http.Get(fmt.Sprintf("https://opt_service/%s", accountID))
+	res, err = http.Get(fmt.Sprintf("https://opt_service/%s", accountID))
 	if err != nil {
 		return false, fmt.Errorf("http error: %w", err)
 	}
+	rtnBytes, err = ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
-	rtnBytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return false, fmt.Errorf("parse response error: %w", err)
 	}
-	var resMap map[string]interface{}
-	json.Unmarshal(rtnBytes, &resMap)
-	currentOtp := resMap["otp"]
+	if res.StatusCode != 200 {
+		return false, fmt.Errorf("unexpected response status code: %d, body %s", res.StatusCode, rtnBytes)
+	}
+	currentOtp := string(rtnBytes)
 
-	if currentOtp != otp {
+	if otp == currentOtp && hashedPwd == pwdFromDB {
+		res, err = http.Get(fmt.Sprintf("https://failed_count/%s/reset", accountID))
+		if err != nil {
+			return false, fmt.Errorf("http error: %w", err)
+		}
+		rtnBytes, err = ioutil.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			return false, fmt.Errorf("parse response error: %w", err)
+		}
+		if res.StatusCode != 200 {
+			return false, fmt.Errorf("unexpected response status code: %d, body %s", res.StatusCode, rtnBytes)
+		}
+
+		return true, nil
+	} else {
+		// get failed count
+		res, err = http.Get(fmt.Sprintf("https://failed_count/%s", accountID))
+		if err != nil {
+			return false, fmt.Errorf("http error: %w", err)
+		}
+		rtnBytes, err = ioutil.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			return false, fmt.Errorf("parse response error: %w", err)
+		}
+		if res.StatusCode != 200 {
+			return false, fmt.Errorf("unexpected response status code: %d, body %s", res.StatusCode, rtnBytes)
+		}
+
+		// log failed count
+		logger := log.New(os.Stderr, "[Debug] ", 0)
+		logger.Printf("failed times: %s", string(rtnBytes))
+
+		// notify slack
+		fmt.Println("this is slack api post")
+
+		// add failed count
+		res, err = http.Get(fmt.Sprintf("https://failed_count/%s/add", accountID))
+		if err != nil {
+			return false, fmt.Errorf("http error: %w", err)
+		}
+		rtnBytes, err = ioutil.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			return false, fmt.Errorf("parse response error: %w", err)
+		}
+		if res.StatusCode != 200 {
+			return false, fmt.Errorf("unexpected response status code: %d, body %s", res.StatusCode, rtnBytes)
+		}
+
 		return false, nil
 	}
-
-	return true, nil
 }
